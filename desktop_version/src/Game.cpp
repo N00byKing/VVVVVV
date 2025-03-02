5587548a25752bd8d33d9f68afb4cbdcdba79b40
Mateon1 <matin1111@wp.pl>
2025-03-02 18:14:18 +0100

    Mark trinket as received just before showing the textbox Fixes trinket count displayed

--- desktop_version/src/Game.cpp
+++ desktop_version/src/Game.cpp
@@ -2411,6 +2411,7 @@ void Game::updatestate(void)
             break;
         case 1001:
             //Found a trinket!
+            V6AP_RecvClear(); // Clear one item
             advancetext = true;
             incstate();
             graphics.createtextboxflipme("", 50, 85, TEXT_COLOUR("gray"));
@@ -2444,7 +2445,6 @@ void Game::updatestate(void)
                 music.fadeMusicVolumeIn(3000);
             }
             graphics.showcutscenebars = false;
-            V6AP_RecvClear(); // Clear one item
             break;
 
         case 1010:
